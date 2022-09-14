f2ed3bac0760f85f79e0995b80f36fa8202cf3b0
tommy6134 <thomas.amerio.ta@gmail.com>
2022-09-14 09:04:29 +0200

    design 0.2

---
+++ app/src/main/java/it/frittomistogames/dicebattles/GameConfigurationActivity.java
@@ -0,0 +1,16 @@
+package it.frittomistogames.dicebattles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class GameConfigurationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); //nasconde la titleBar e la appBar
+        setContentView(R.layout.activity_game_configuration);
+    }
+}
